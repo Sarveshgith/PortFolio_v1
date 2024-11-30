8a41da93a4ce920a7bcae501ebc50cb84cf164af
Sarveshgith <130198294+Sarveshgith@users.noreply.github.com>
2024-11-30 15:11:48 +0530

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import Hero from './components/hero';
 import Skills from './components/Skills';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import MarqueeDemo from './components/Marquee';
 const App = () => {
 	return (
 		<BrowserRouter>
 			<div>
-				{/* <Hero /> */}
-				{/* <MarqueeDemo /> */}
-				{/* <Skills /> */}
 				<Routes>
 					<Route path='/' element={<Hero />} />
 					<Route path='/acq' element={<Skills />} />
@@ -20,25 +16,3 @@ const App = () => {
 
 export default App;
 
-// const slugs = [
-// 	'javascript',
-// 	'java',
-// 	'react',
-// 	'html5',
-// 	'css3',
-// 	'nodedotjs',
-// 	'express',
-// 	'postgresql',
-// 	'firebase',
-// 	'git',
-// 	'github',
-// 	'visualstudiocode',
-// 	'c',
-// 	'c++',
-// 	'python',
-// 	'django',
-// 	'perl',
-// 	'mongodb',
-// 	'springboot',
-// 	'mysql',
-// ];
